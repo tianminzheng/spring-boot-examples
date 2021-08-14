1d18d8c7740edb15237c87c2c73e9c0fa0e767b3
天涯兰 <lantian0825@163.com>
2021-08-14 14:08:48 +0800

    Add files via upload

---
+++ SpringAuthCacheExample/src/main/resources/authorities.sql
@@ -0,0 +1,4 @@
+                                                 
+INSERT INTO authorities(username, authority) VALUES ('customer', 'USER'); 
+INSERT INTO authorities(username, authority) VALUES ('manager', 'USER');
+INSERT INTO authorities(username, authority) VALUES ('manager', 'ADMIN');
---
+++ SpringAuthCacheExample/src/main/resources/users.sql
@@ -0,0 +1,5 @@
+--user: customer; password: Test@123
+INSERT INTO users(username, password, enabled) VALUES ('customer', '$2a$10$P.2FmSPSL6nt7BQmAyWhv.Z2g5Gy0jMDcHpA6UlfEmgMKgz2yL4Pu', true);
+
+--user: manager; password: Test@123
+INSERT INTO users(username, password, enabled) VALUES ('manager', '$2a$10$P.2FmSPSL6nt7BQmAyWhv.Z2g5Gy0jMDcHpA6UlfEmgMKgz2yL4Pu', true);
