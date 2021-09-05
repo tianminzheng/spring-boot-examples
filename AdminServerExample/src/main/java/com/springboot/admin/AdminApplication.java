10715c29fba6a6366c792485bc49bc83f387c45a
天涯兰 <lantian0825@163.com>
2021-09-05 19:52:29 +0800

    Add files via upload

---
+++ AdminServerExample/src/main/java/com/springboot/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
